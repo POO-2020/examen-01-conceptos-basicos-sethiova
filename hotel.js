340543664baa00d3c113a9df94533c42c34c696e
Maximiliano Alexander Mendoza López <mmendoza34@ucol.mx>
2020-03-03 18:51:02 -0600

    Se han realizado modifcaciones

--- hotel.js
+++ hotel.js
@@ -22,20 +22,3 @@ export default class Hotel {
         })
     }
 }
-let prueba = new Hotel("Las Brisas")
-let reservacion1 = new Reservacion(1234, new Date(2003,4,23), 5) 
-        let huesped1 = new Huesped("Jose Manuel Alcantar", "Masculino")
-        let huesped2 = new Huesped("Maria Regina Mariel", "Femenino")
-        reservacion1.addHuesped(huesped1)
-        reservacion1.addHuesped(huesped2)
-        prueba.addReservacion(reservacion1)
-        let reservacion2 = new Reservacion(1222, new Date(2005,6,13), 5) 
-        let huesped3 = new Huesped("Alan Ugarte Saucedo", "Masculino")
-        let huesped4 = new Huesped("Daniela Covarrubias Mendoza", "Femenino")
-        reservacion2.addHuesped(huesped3)
-        reservacion2.addHuesped(huesped4)
-        prueba.addReservacion(reservacion2)
-
-        console.log(prueba.printHotel())
-        console.log(prueba.getNumHuespedes2())
-        
--- main.js
+++ main.js
@@ -7,11 +7,23 @@ class Main {
         this.hotel = new Hotel("Las Brisas")
     }
 
-    addReservacion() {
-        
+   prueba() {
+        let reservacion1 = new Reservacion(1234, new Date(2003,4,23), 5) 
+        let huesped1 = new Huesped("Jose Manuel Alcantar", "Masculino")
+        let huesped2 = new Huesped("Maria Regina Mariel", "Femenino")
+        reservacion1.addHuesped(huesped1)
+        reservacion1.addHuesped(huesped2)
+        this.hotel.addReservacion(reservacion1)
+        let reservacion2 = new Reservacion(1222, new Date(2005,6,13), 5) 
+        let huesped3 = new Huesped("Alan Ugarte Saucedo", "Masculino")
+        let huesped4 = new Huesped("Daniela Covarrubias Mendoza", "Femenino")
+        reservacion2.addHuesped(huesped3)
+        reservacion2.addHuesped(huesped4)
+        this.hotel.addReservacion(reservacion2)
         this.hotel.printHotel()
     }    
 }
 
 
 let app = new Main
+console.log(app.prueba())
